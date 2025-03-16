9866a333cd936a69802ac62a21471ed96b97166f
iburzynski <23251244+iburzynski@users.noreply.github.com>
2025-03-16 19:59:17 -0400

    Complete task 3 (output formatting)

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyse):
@@ -5,4 +6,13 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     my_obj = { "raw_document": { "text": text_to_analyse } }
     resp = requests.post(url=url, headers=header, json=my_obj)
-    return resp.text
+    resp_json = json.loads(resp.text)
+    emotions = resp_json.get("emotionPredictions")[0].get("emotion")
+    max_score = 0
+    dominant_emotion = None
+    for emotion, score in emotions.items():
+        if score > max_score:
+            max_score = score
+            dominant_emotion = emotion
+    emotions["dominant_emotion"] = dominant_emotion
+    return emotions
