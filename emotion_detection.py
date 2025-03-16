bd191cbdae54fb9e332c6e85a9fdee0ff7705976
iburzynski <23251244+iburzynski@users.noreply.github.com>
2025-03-16 19:39:44 -0400

    Complete task 2 (emotion_detector)

---
+++ emotion_detection.py
@@ -0,0 +1,8 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    my_obj = { "raw_document": { "text": text_to_analyse } }
+    resp = requests.post(url=url, headers=header, json=my_obj)
+    return resp.text
