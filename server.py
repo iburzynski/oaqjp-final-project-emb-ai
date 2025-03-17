053560e3b917e3312afafd946a3316a4dddee0dc
iburzynski <23251244+iburzynski@users.noreply.github.com>
2025-03-16 21:11:23 -0400

    Complete task 8 (static analysis)

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+'''
+Flask server for Emotion Analysis.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def detect():
+    '''
+    Runs emotion analysis on user submitted form string.
+    '''
     text_to_analyze = request.args["textToAnalyze"]
     result = emotion_detector(text_to_analyze)
     anger = result.get("anger")
@@ -13,17 +20,24 @@ def detect():
     joy = result.get("joy")
     sadness = result.get("sadness")
     dom_emote = result.get("dominant_emotion")
-    sys_resp = f'''
+    if not dom_emote:
+        sys_resp = "Invalid text! Please try again!"
+    else:
+        sys_resp = f'''
         For the given statement, the system response is 'anger': {anger},
         'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 
         'sadness': {sadness}. The dominant emotion is 
         <strong>{dom_emote}</strong>.
-    '''
+        '''
     return sys_resp
 
 @app.route("/")
 def index():
+    '''
+    Renders the index page.
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
